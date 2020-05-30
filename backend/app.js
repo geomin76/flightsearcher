7a58ebd0cd44677c7db8aac5c97e951a529e5ac9
geomin76 <geom@vt.edu>
2020-05-30 17:53:31 -0400

    api set up

--- backend/app.js
+++ backend/app.js
@@ -2,24 +2,32 @@ const express = require('express');
 const app = express();
 var secrets = require('./secrets.js');
 const request = require('request');
+var unirest = require("unirest");
 
 app.get('/', (req, res) => {
     res.send("Hello, World!")
 })
 
 app.get('/createSession', (req, res) => {
-    /**
-     * https://partners.api.skyscanner.net/apiservices/pricing/v1.0
-     * Params required:
-     * - Country
-     * - Currency
-     * - Locale
-     * - originPlace
-     * - destinationPLace
-     * - outboundDate
-     * - adults
-     * - apiKey
-     */
+
+    var req = unirest("GET", "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/UK/GBP/en-GB/");
+
+    req.query({
+        "query": "Stockholm"
+    });
+
+    req.headers({
+        "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+        "x-rapidapi-key": secrets.key,
+        "useQueryString": true
+    });
+
+
+    req.end(function (res) {
+        if (res.error) throw new Error(res.error);
+
+        console.log(res.body);
+    });
 })
 
 
