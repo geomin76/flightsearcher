3d32759a83a1d9576e76c21729a0260a29783005
geomin76 <geom@vt.edu>
2020-06-13 22:30:58 -0400

    dynamic date added

--- frontend/src/app/query/query.component.ts
+++ frontend/src/app/query/query.component.ts
@@ -4,25 +4,32 @@ import { Search } from '../search';
 import { Router } from '@angular/router';
 import { DatarequestService } from '../datarequest.service';
 import { GooglePlaceDirective } from 'ngx-google-places-autocomplete';
-import { LatLng } from 'ngx-google-places-autocomplete/objects/latLng';
-import { Geometry } from 'ngx-google-places-autocomplete/objects/geometry';
+import { DatePipe } from '@angular/common'
+import { toInteger } from '@ng-bootstrap/ng-bootstrap/util/util';
 
 @Component({
   selector: 'app-query',
   templateUrl: './query.component.html',
-  styleUrls: ['./query.component.css']
+  styleUrls: ['./query.component.css'],
+  providers: [DatePipe]
 })
 export class QueryComponent implements OnInit {
 
   searchModel = new Search('', '');
 
-  constructor(private dataService : DatarequestService, private router: Router) { }
+  constructor(private dataService : DatarequestService, private router: Router, private datePipe: DatePipe) { }
 
   public lat = 0;
   public lng = 0;
 
+  public currentDate = new Date();
+  public preDate = '';
+  public postDate = '';
 
   ngOnInit(): void {
+    this.preDate = this.datePipe.transform(this.currentDate, 'yyyy-MM');
+    var datesplit = this.preDate.split('-');
+    this.postDate = (parseInt(datesplit[0]) + 1).toString() + "-" + datesplit[1]
   }
 
 
