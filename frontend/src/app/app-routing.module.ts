0731f69ff6a11e62bcd314f172f825587e8dc85e
geomin76 <geom@vt.edu>
2020-06-13 22:49:43 -0400

    no flight error msg

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { QueryComponent } from './query/query.component';
 import { ResultsComponent } from './results/results.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
-
 const routes: Routes = [
   { path: '', redirectTo: '/query', pathMatch: 'full'},
   { path: 'query', component: QueryComponent},
