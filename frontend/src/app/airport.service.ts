0ca7b0f34d9eef6748f59493fcc338f85686607a
geomin76 <geom@vt.edu>
2020-07-08 20:54:46 -0400

    instances up

--- frontend/src/app/airport.service.ts
+++ frontend/src/app/airport.service.ts
@@ -9,7 +9,7 @@ export class AirportService {
   constructor(private http: HttpClient) { }
 
   public getAirports(query) {
-    var url = "http://localhost:3000/search?name=" + query;
+    var url = "http://ec2-54-85-15-137.compute-1.amazonaws.com:3000/search?name=" + query;
     return this.http.get(url);
   }
 }
--- frontend/src/app/datarequest.service.ts
+++ frontend/src/app/datarequest.service.ts
@@ -11,7 +11,7 @@ export class DatarequestService {
   constructor(private http: HttpClient) { }
 
   public getFlights(lng, lat, destination, date) {
-    var url = "http://localhost:3000/results?lng=" + lng + "&lat=" + lat + "&destination=" + destination + "&outbound=" + date +"&inbound=" + date;
+    var url = "http://ec2-54-85-15-137.compute-1.amazonaws.com:3000/results?lng=" + lng + "&lat=" + lat + "&destination=" + destination + "&outbound=" + date +"&inbound=" + date;
     return this.http.get(url);
   }
 
